d2202a73fc5f7931223544fb2e4b9912a10dd080
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2024-06-08 16:00:44 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+  return render_template('inicio.html')
+
+@app.route('/criar_pedido)
+def criar_pedido():
+  return render_template('criar_pedido.html')
+
+@app.route('/finalizacao')
+def finalizacao():
+  return render_template('finalizar.html')
+  
+if __name__ == '__main__':
+    app.run(debug=True)
